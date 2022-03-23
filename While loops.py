cb1e42a26fbd2bcc69262305989142fc3c8bdfb4
Lolmaster21 <78552751+Lolmaster21@users.noreply.github.com>
2022-03-23 13:59:56 -0600

    Create While loops.py

---
+++ While loops.py
@@ -0,0 +1,11 @@
+i = 0
+while i < 23:
+    print (i)
+    i+=3
+num = 0
+num = (int(input("How many times do you want to out snurfle???????????????")))
+
+j=0
+while (j != num) :
+    print ("smu")
+    j+=1
