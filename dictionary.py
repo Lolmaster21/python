9a34c4e53de9bfa15721a98bfe4d0d0dc7346108
Lolmaster21 <78552751+Lolmaster21@users.noreply.github.com>
2022-10-05 09:53:19 -0600

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,17 @@
+Snarfbooger = {"Fried BALAGNA": 4.75,
+             "French Fries": 3.05,
+             "Grilled Cheese": 3.25,
+             "Hot doy": 5.50,
+             "B.L.T": 6.66}#DICTIONARY
+
+
+
+input = 0
+
+while input != 'q':
+    print("The menu is: ",Snarfbooger )
+    print(int(input))
+
+    
+else:
+    print("Thats not an option >:|")
