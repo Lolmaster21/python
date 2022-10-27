ba949a1a64391dd4753f80b2698dcc1eec4c3770
Lolmaster21 <78552751+Lolmaster21@users.noreply.github.com>
2022-10-26 22:54:43 -0600

    Create MID term.py

---
+++ MID term.py
@@ -0,0 +1,51 @@
+import math
+
+print("Blue or green?")
+
+choice = input()
+
+if choice == 'blue':
+    print("The sky is blue")
+elif choice == 'green':
+    print("The grass is green")
+else:
+    print("Thats not an option dummy")
+
+
+
+
+
+print("How old is u?")
+a=int(input())
+
+if a<43:
+        print("You're to young go watch some tak tik")
+elif a==43:
+        print("Ah yes the perfect number")
+else:
+    print("Youre to old go eat some oatmeal")
+    
+    
+
+for i in range(50,30,-3):
+    print(i)
+#----------------------------------------------------------------------
+print("new loop\n")
+t= 10
+while t<= 60:
+  print (t)
+  t+=5
+#-----------------------------------------------------------------------  
+
+words = input
+while words != "stop":
+    words = input("Enter a word any word(Print stop to quit)")
+
+#----------------------------------------------------------------------
+
+def division(x,y):
+    return x/y
+print(int(input("Give me two numbers")))
+input = x
+input = y
+print(division(x,y))
