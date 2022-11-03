2697d4625dfb3f6864f2c94efaf5ed1b5040468d
Lolmaster21 <78552751+Lolmaster21@users.noreply.github.com>
2022-11-03 09:51:35 -0600

    Create Function sort.py

---
+++ Function sort.py
@@ -0,0 +1,17 @@
+print("Spam random number put 0 to quit")
+def number(x):
+    print(list)
+    list.sort()
+    
+    print("The largest number item in your list is:", list[-1])
+    
+    
+list = []
+while True:
+    num = int(input())
+    if num != 0:
+        list.append(num)
+    else:
+        break
+
+number(list)
