f80a055bb15cd80fb04ee58b4e583d397a31b73c
Lolmaster21 <78552751+Lolmaster21@users.noreply.github.com>
2022-12-05 09:06:45 -0700

    Update shope.py

--- shope.py
+++ shope.py
@@ -4,30 +4,45 @@ coins = 100
 def shoppe(coins):
     stuff = items
     
-    while coins > 0:
+    while coins > 0 :
          print("Welcome to the shop you have ", coins, "coins")
          print("Your items are:", stuff)
          print("Todays scam is: Potion:$20, Food: $15, Keys: $30 ")
-         choice = input("press p for potion, press f for food, and k for keys")
+         choice = input("press p for potion, press f for food, and k for keys. Press q to quit")
          
          if choice =="p":
-             print("You got a potion ")
-             items[0] = "Potion"
-             coins -= 20
-             return coins
+             if coins >= 20:
+                 print("You got a potion ")
+                 items[0] = "Potion"
+                 coins -= 20
+             else:
+                  print("You broke")
+                  return coins
+
 
                 
-         if choice == "f":
-             print("You got food")
-             coins -= 15
-             items[1] = "food"
-             return coins
+         elif choice == "f":
+              if coins >= 15:
+                 print("You got food")
+                 items[1] = "food"
+                 coins -= 15
+              else:
+                  print("You broke")
+                  return coins
+
 
                 
-         if choice == "k":
-             print("You got a key")
-             coins -=30
-             items[2] = "key"
+         elif choice == "k":
+              if coins >= 30:
+                 print("You got a key")
+                 items[2] = "key"
+                 coins -= 30
+              else:
+                  print("You broke")
+                  return coins
+                
+         elif choice == "q":
+             print("Goodbye")
              return coins
 
          else:
@@ -38,6 +53,4 @@ def shoppe(coins):
               
 
 
-print (shoppe(coins))
-    
-    
+coins = shoppe(coins)
